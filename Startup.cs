a44aa6576307e6e67e61c5dd591366b1428e6c06
TinDinh99 <64943879+TinDinh99@users.noreply.github.com>
2020-09-19 00:32:03 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DuongTinShop.Startup))]
+namespace DuongTinShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
